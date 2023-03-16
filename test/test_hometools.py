a94bcbdd1fcec1b39d6183f28d71052eb9adfab4
mnshgl0110 <mnshgl0110@gmail.com>
2023-03-16 16:03:55 +0100

    Changed python version used for testing

--- test/test_hometools.py
+++ test/test_hometools.py
@@ -1,9 +1,11 @@
 def test_import():
     from hometools.hometools import readfasta
     assert True
+# END
+
 
 def test_cli():
     from subprocess import Popen, PIPE
-    p=Popen('hometools -h'.split(), stdout=PIPE, stderr=PIPE)
-    out=p.communicate()
+    p = Popen('hometools -h'.split(), stdout=PIPE, stderr=PIPE)
+    out = p.communicate()
     assert 'error' not in out[1].decode()
