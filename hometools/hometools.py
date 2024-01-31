27c0b88a72f0d9a56dc340e1a0a02a5197d54171
mnshgl0110 <mnshgl0110@gmail.com>
2024-01-31 19:40:59 +0100

    handle bamrc with rc=0

--- hometools/hometools.py
+++ hometools/hometools.py
@@ -1802,7 +1802,7 @@ def bamrc2af(args):
     with open(rcfin, 'r') as rc, open(outfin, 'w') as out:
         for line in rc:
             line = line.strip().split()
-            if line[3] == 0: continue
+            if line[3] == '0': continue
             try:
                 ref, alt = posdict[(line[0], line[1])]
             except KeyError:
